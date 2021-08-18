2e5f72bce6a1ce7288548f368b9fff76f8210929
gus <kevinlichen1412@gmail.com>
2021-08-18 22:38:30 +0630

    Rechange

--- Pass-10/show_code.php
+++ Pass-10/show_code.php
@@ -4,17 +4,17 @@
 <code class="line-numbers language-php">$is_upload = false;
 $msg = null;
 if (isset($_POST['submit'])) {
-    if (file_exists(UPLOAD_PATH)) {
+    if (file_exists($UPLOAD_ADDR)) {
         $deny_ext = array(".php",".php5",".php4",".php3",".php2",".html",".htm",".phtml",".pht",".pHp",".pHp5",".pHp4",".pHp3",".pHp2",".Html",".Htm",".pHtml",".jsp",".jspa",".jspx",".jsw",".jsv",".jspf",".jtml",".jSp",".jSpx",".jSpa",".jSw",".jSv",".jSpf",".jHtml",".asp",".aspx",".asa",".asax",".ascx",".ashx",".asmx",".cer",".aSp",".aSpx",".aSa",".aSax",".aScx",".aShx",".aSmx",".cEr",".sWf",".swf",".htaccess",".ini");
         $file_name = trim($_FILES['upload_file']['name']);
         $file_ext = str_ireplace('::$DATA', '', $file_ext);//Removing strings::$DATA
         
-        if (move_uploaded_file($_FILES['upload_file']['tmp_name'], UPLOAD_PATH . '/' . $file_name)) {
-            $img_path = UPLOAD_PATH . '/' .$file_name;
+        if (move_uploaded_file($_FILES['upload_file']['tmp_name'], $UPLOAD_ADDR . '/' . $file_name)) {
+            $img_path = $UPLOAD_ADDR . '/' .$file_name;
             $is_upload = true;
         }
     } else {
-        $msg = UPLOAD_PATH . 'Folder does not exist, please create it manually！';
+        $msg = $UPLOAD_ADDR . 'Folder does not exist, please create it manually！';
     }
 }
 </code>
