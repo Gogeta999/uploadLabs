004d15319c0326ea507a0ea6db92d90acc77e7fd
Ash <kevinlichen1412@gmail.com>
2021-08-21 17:46:21 +0000

    change 9

--- Pass-09/show_code.php
+++ Pass-09/show_code.php
@@ -7,7 +7,8 @@ $msg = null;
 if (isset($_POST['submit'])) {
     if (file_exists(UPLOAD_PATH)) {
         $deny_ext = array(".php",".php5",".php4",".php3",".php2",".html",".htm",".phtml",".pht",".pHp",".pHp5",".pHp4",".pHp3",".pHp2",".Html",".Htm",".pHtml",".jsp",".jspa",".jspx",".jsw",".jsv",".jspf",".jtml",".jSp",".jSpx",".jSpa",".jSw",".jSv",".jSpf",".jHtml",".asp",".aspx",".asa",".asax",".ascx",".ashx",".asmx",".cer",".aSp",".aSpx",".aSa",".aSax",".aScx",".aShx",".aSmx",".cEr",".sWf",".swf",".htaccess");
-        $file_name = deldot($file_name);//Delete dot
+	$file_name = $_FILES['upload_file']['name'];
+	$file_name = deldot($file_name);//Delete dot
         $file_ext = strrchr($file_name, '.');
         $file_ext = strtolower($file_ext); //Conver To Small Letter
         $file_ext = str_ireplace('::$DATA', '', $file_ext);//Remove String::$DATA
