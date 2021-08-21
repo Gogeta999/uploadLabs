fa582071e1055e50efa638a567988cdcf21b1e59
gus <kevinlichen1412@gmail.com>
2021-08-22 01:51:41 +0630

    Ｃｈａｎｇｅ　Ｐａｓｓ　６　ｔｏ　ｚｋａｑ

--- Pass-06/show_code.php
+++ Pass-06/show_code.php
@@ -6,20 +6,17 @@ $is_upload = false;
 $msg = null;
 if (isset($_POST['submit'])) {
     if (file_exists(UPLOAD_PATH)) {
-        $deny_ext = array(".php",".php5",".php4",".php3",".php2",".html",".htm",".phtml",".pht",".pHp",".pHp5",".pHp4",".pHp3",".pHp2",".Html",".Htm",".pHtml",".jsp",".jspa",".jspx",".jsw",".jsv",".jspf",".jtml",".jSp",".jSpx",".jSpa",".jSw",".jSv",".jSpf",".jHtml",".asp",".aspx",".asa",".asax",".ascx",".ashx",".asmx",".cer",".aSp",".aSpx",".aSa",".aSax",".aScx",".aShx",".aSmx",".cEr",".sWf",".swf",".htaccess");
-        $file_name = $_FILES['upload_file']['name'];
+	$deny_ext = array(".php",".php5",".php4",".php3",".php2",".html",".htm",".phtml",".pHp",".pHp5",".pHp4",".pHp3",".pHp2",".Html",".Htm",".pHtml",".jsp",".jspa",".jspx",".jsw",".jsv",".jspf",".jtml",".jSp",".jSpx",".jSpa",".jSw",".jSv",".jSpf",".jHtml",".asp",".aspx",".asa",".asax",".ascx",".ashx",".asmx",".cer",".aSp",".aSpx",".aSa",".aSax",".aScx",".aShx",".aSmx",".cEr",".sWf",".swf",".htaccess");    
+	$file_name = $_FILES['upload_file']['name'];
         $file_name = deldot($file_name);//Delete dot in name's latest place
         $file_ext = strrchr($file_name, '.');
         $file_ext = strtolower($file_ext); //Convert to lowercase
         $file_ext = str_ireplace('::$DATA', '', $file_ext);//Remove String::$DATA
         
         if (!in_array($file_ext, $deny_ext)) {
-            $temp_file = $_FILES['upload_file']['tmp_name'];
-            $img_path = UPLOAD_PATH.'/'.date("YmdHis").rand(1000,9999).$file_ext;
-            if (move_uploaded_file($temp_file,$img_path)) {
+            if (move_uploaded_file($_FILES['upload_file']['tmp_name'], UPLOAD_PATH . '/' . $_FILES['upload_file']['name'])) {
+                $img_path = UPLOAD_PATH . '/' . $file_name;
                 $is_upload = true;
-            } else {
-                $msg = 'Upload Failed!';
             }
         } else {
             $msg = 'This file is not allowed to be uploaded!';
