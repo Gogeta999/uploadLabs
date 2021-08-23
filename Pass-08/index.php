aa4105a5fdc095d9b700542c83b46cfb415737fb
gus <kevinlichen1412@gmail.com>
2021-08-23 19:00:58 +0630

    Change TO 5.2

--- Pass-08/index.php
+++ Pass-08/index.php
@@ -7,7 +7,7 @@ include '../menu.php';
 $is_upload = false;
 $msg = null;
 if (isset($_POST['submit'])) {
-    if (file_exists($UPLOAD_ADDR)) {
+    if (file_exists(UPLOAD_PATH)) {
         $deny_ext = array(".php",".php5",".php4",".php3",".php2",".html",".htm",".phtml",".pht",".pHp",".pHp5",".pHp4",".pHp3",".pHp2",".Html",".Htm",".pHtml",".jsp",".jspa",".jspx",".jsw",".jsv",".jspf",".jtml",".jSp",".jSpx",".jSpa",".jSw",".jSv",".jSpf",".jHtml",".asp",".aspx",".asa",".asax",".ascx",".ashx",".asmx",".cer",".aSp",".aSpx",".aSa",".aSax",".aScx",".aShx",".aSmx",".cEr",".sWf",".swf",".htaccess",".ini");
         $file_name = trim($_FILES['upload_file']['name']);
         $file_name = deldot($file_name);//Remove deldot
@@ -16,15 +16,15 @@ if (isset($_POST['submit'])) {
         $file_ext = trim($file_ext); //Clean up empty
         
         if (!in_array($file_ext, $deny_ext)) {
-            if (move_uploaded_file($_FILES['upload_file']['tmp_name'], $UPLOAD_ADDR . '/' . $_FILES['upload_file']['name'])) {
-                $img_path = $UPLOAD_ADDR . '/' . $file_name;
+            if (move_uploaded_file($_FILES['upload_file']['tmp_name'], UPLOAD_PATH . '/' . $_FILES['upload_file']['name'])) {
+                $img_path = UPLOAD_PATH . '/' . $file_name;
                 $is_upload = true;
             }
         } else {
             $msg = 'This file type is not allowed to be uploaded!';
         }
     } else {
-        $msg = $UPLOAD_ADDR . 'Folder does not exist, please create it manually！';
+        $msg = UPLOAD_PATH . 'Folder does not exist, please create it manually！';
     }
 }
 ?>
